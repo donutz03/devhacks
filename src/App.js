5616bebbe8331a85c48e5f4ad83505df21342194
ionuth <ionut.hodoroaga@capgemini.com>
2022-11-19 21:59:07 +0200

    navbar

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import './App.css';
+import Navbar from './Component/Navbar'
+import Home from './Component/Home'
+import {Routes, Route} from 'react-router-dom'
+import Products from './Component/Products'
+
+function App() {
+  return (
+    <>
+    <Navbar />
+    <Routes>
+      <Route exact path = "/" element = {Home}/>
+      <Route exact path = "/products" element = {Products}/>
+    </Routes>
+    <Home />
+    </>
+  );
+}
+
+export default App;
